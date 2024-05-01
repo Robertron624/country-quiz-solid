cb080bfd6b435e7a1d2aa06fe0aeeaaa91f2a369
Roberto Ramirez <robert.624@hotmail.com>
2024-05-01 14:53:22 -0500

    initial container
    container with numbers and title

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,63 @@
-import type { Component } from 'solid-js';
+import type { Component } from "solid-js";
+import { createSignal, createMemo } from "solid-js";
 
-import styles from './App.module.scss';
+import "./App.scss";
+
+const NOMBER_OF_QUESTIONS = 10;
+const numbers = Array.from({ length: NOMBER_OF_QUESTIONS }, (_, i) => i + 1);
+
+interface CircleNumberProps {
+  circleNumber: number;
+  onNumberClick: (event: Event) => void;
+  currentQuestion: () => number;
+}
+
+const CircleNumber: Component<CircleNumberProps> = ({
+  circleNumber,
+  onNumberClick,
+  currentQuestion,
+}: CircleNumberProps) => {
+  // active state if the number is equal or less than the current question
+  const isButtonActive = createMemo(() => circleNumber <= currentQuestion());
+
+  return (
+    <button
+      class={`circle-number flex justify-center align-center ${isButtonActive() ? "active" : ""}
+    `}
+      onClick={onNumberClick}
+      data-question={circleNumber}
+    >
+      {circleNumber}
+    </button>
+  );
+};
 
 const App: Component = () => {
+  const [currentQuestion, setCurrentQuestion] = createSignal(1);
+
+  function handleNumberClick(event: Event) {
+    const target = event.target as HTMLButtonElement;
+    const questionNumber = parseInt(target.dataset.question || "1", 10);
+
+    setCurrentQuestion(questionNumber);
+  }
+
   return (
-    <div class={styles.App}>
-        <h1>
-          Hello world!
-        </h1>
+    <div class={`App`}>
+      <div class={`max-w-800 container`}>
+        <h1>Country Quiz</h1>
+        <div class='numbers flex'>
+          {numbers.map((number) => {
+            const buttonProps = {
+              circleNumber: number,
+              onNumberClick: handleNumberClick,
+              currentQuestion,
+            };
+
+            return <CircleNumber {...buttonProps} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 };
